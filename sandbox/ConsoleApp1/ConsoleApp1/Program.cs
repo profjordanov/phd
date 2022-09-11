394868644da4ddee0cd14b57624f48dab9e35cb6
Jordan Jordanov <d_yugioh@abv.bg>
2022-09-11 14:10:16 +0300

    v1.0.2

---
+++ sandbox/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Start");
+
+var records = File.ReadAllLines("benl.csv");
+
+var results = records
+    .Select((x, i) => new { Index = i, Value = x })
+    .GroupBy(x => x.Index / 380)
+    .Select(x => x.Select(v => v.Value).ToList())
+    .ToList();
+
+for (int i = 0; i < results.Count; i++)
+{
+    File.WriteAllLines($"./batch{i+1}.csv", results[i]);
+    Console.WriteLine($"./batch{i+1}.csv");
+}
+
+Console.WriteLine("Done");
