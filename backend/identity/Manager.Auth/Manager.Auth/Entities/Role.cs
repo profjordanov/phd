f36afd9fd6751b3fc6cb8f36f1be32d642ac04ad
Jordan Jordanov <d_yugioh@abv.bg>
2022-09-12 12:13:45 +0300

    v1.0.3

--- backend/identity/Manager.Auth/Manager.Auth/Entities/Role.cs
+++ backend/identity/Manager.Auth/Manager.Auth/Entities/Role.cs
@@ -4,5 +4,22 @@ namespace Manager.Auth.Entities;
 
 public class Role : IdentityRole<Guid>
 {
-    
+    public override Guid Id { get; set; }
+    public override string Name { get; set; }
+    public override string ConcurrencyStamp { get; set; }
+    public override string NormalizedName { get; set; }
+    public override string ToString()
+    {
+        return base.ToString();
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return base.Equals(obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
+    }
 }
